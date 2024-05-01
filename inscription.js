8c9dd63033ecca4b8823f95b0af5e8a58bd0f330
Charlyrobert <155687156+Charlyrobert@users.noreply.github.com>
2024-05-01 19:37:51 +0200

    Update inscription.js

--- inscription.js
+++ inscription.js
@@ -17,3 +17,18 @@ function validatePassword() {
 // Écouter les événements de saisie dans les champs de mot de passe
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+
+
+// Récupérer le formulaire d'inscription
+var signupForm = document.getElementById("registration_form");
+
+// Écouter l'événement de soumission du formulaire
+signupForm.addEventListener("submit", function(event) {
+    // Empêcher le comportement par défaut du formulaire (éviter le rechargement de la page)
+    event.preventDefault();
+
+
+    // Une fois le formulaire validé, rediriger l'utilisateur vers la page de profil
+    window.location.href = "profil.html";
+});
