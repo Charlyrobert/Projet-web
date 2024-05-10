1f2c0b86e64981fed3bdd29d81bd7a8e1ede5402
Charlyrobert <155687156+Charlyrobert@users.noreply.github.com>
2024-05-10 14:51:36 +0200

    Update profil.js

--- profil.js
+++ profil.js
@@ -1,13 +1,31 @@
-// Récupérer les données utilisateur depuis le localStorage
-var userData = JSON.parse(localStorage.getItem("userData"));
+// Profil.js
 
-// Vérifier si des données utilisateur existent dans le localStorage
-if (userData) {
-    // Afficher les données utilisateur dans les éléments correspondants de la page de profil
-    document.getElementById("pseudo").innerText = userData.pseudo;
-    document.getElementById("sexe").innerText = userData.sexe;
-    // AJOUTER LES AUTRES
-} else {
-    // Si aucune donnée utilisateur n'est trouvée dans le localStorage, rediriger l'utilisateur vers la page d'inscription
-    window.location.href = "inscription.html";
+// Fonction pour récupérer les informations de l'utilisateur depuis le stockage local
+function afficherProfilUtilisateur() {
+    // Récupérer les données de l'utilisateur depuis le stockage local
+    const profilUtilisateur = JSON.parse(localStorage.getItem("utilisateur"));
+
+    // Vérifier si des données sont disponibles
+    if (profilUtilisateur) {
+        // Afficher les informations dans la page
+        pseudoElement.textContent = profilUtilisateur.pseudo;
+        sexeElement.textContent = profilUtilisateur.sexe;
+        nomElement.textContent = profilUtilisateur.nom;
+        adresseElement.textContent = profilUtilisateur.adresse;
+        passwordElement.textContent = profilUtilisateur.password;
+
+        // Afficher les fichiers téléchargés
+        profilUtilisateur.fichiers.forEach(fichier => {
+            const lienFichier = document.createElement("a");
+            lienFichier.href = fichier;
+            lienFichier.textContent = fichier;
+            fichiersElement.appendChild(lienFichier);
+            fichiersElement.appendChild(document.createElement("br"));
+        });
+    } else {
+        console.log("Aucun profil utilisateur trouvé !");
+    }
 }
+
+// Appeler la fonction pour afficher le profil de l'utilisateur
+afficherProfilUtilisateur();
