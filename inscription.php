bd623bae19ece65a97d76fa102461273a45afc2e
Charlyrobert <155687156+Charlyrobert@users.noreply.github.com>
2024-05-23 22:48:09 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -11,18 +11,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $region = $_POST["region"];
     $bio = $_POST["bio"];
     
-/*
-    $departement = $_POST["departement"];
-    $situation_amoureuse = $_POST["situation_amoureuse"];
-    $taille = $_POST["taille"];
-    $poids = $_POST["poids"];
-    $centres_interets = $_POST["centres_interets"];
-*/
 
     // Stocker les données dans un fichier texte
     $file = fopen("utilisateurs.txt", "a");
-    fwrite($file, "$pseudo | $password | $sexe | $dob | $profession | $pays | $region | $bio \n");    
-//    fwrite($file, "Post $pseudo | $password | $sexe | $dob | $profession | $pays | $region | $departement | $situation_amoureuse | $taille | $poids | $bio | $centres_interets\n");
+
+    $lines = file("utilisateurs.txt");
+    foreach ($lines as $line) {
+        $infos = explode(" | ", $line);
+        if ($infos[0] == $pseudo) {
+            $existe = true;
+        } else {
+            $existe = false;
+        }
+    }
+
+    if ($existe == false){
+        fwrite($file, "$pseudo | $password | $sexe | $dob | $profession | $pays | $region | $bio \n");    
+    }
+
+
     fclose($file);
 
     // Rediriger l'utilisateur vers la page de profil après l'inscription
