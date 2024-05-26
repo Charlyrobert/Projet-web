a90712a23f97d08411f2a1ad2fe4bf6409507eab
MarvynZZZ <133578489+MarvynZZZ@users.noreply.github.com>
2024-05-26 18:53:00 +0200

    Create fetchuser.php

---
+++ fetchuser.php
@@ -0,0 +1,17 @@
+<?php
+$file = 'SMS/utilisateurs';
+
+if (file_exists($file)) {
+    $lines = file($file);
+    $result = '';
+
+    foreach ($lines as $line) {
+        list($firstField) = explode('|', $line);
+        $result .= htmlspecialchars(trim($firstField)) . '<br>';
+    }
+
+    echo $result ? $result : 'Aucun utilisateur trouvé.';
+} else {
+    echo 'Fichier introuvable.';
+}
+?>
